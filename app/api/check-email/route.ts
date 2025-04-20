bd51ad00460222be42212f7a668da14df97c3211
Mayank <mayankgpt185@gmail.com>
2025-04-20 18:03:19 +0530

    fixed import

--- app/api/check-email/route.ts
+++ app/api/check-email/route.ts
@@ -1,12 +1,12 @@
 import dbConnect from '@/lib/mongodb';
 import { NextResponse } from 'next/server';
-import Staff from '../models/staff';
+import User from '../models/user';
 
 export async function POST(request: Request) {
   try {
     await dbConnect();
     const { email } = await request.json();
-    const exists = await Staff.findOne({ email });
+    const exists = await User.findOne({ email });
     return NextResponse.json(!!exists);
   } catch (error) {
     return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
