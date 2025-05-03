df0a5873b83cf047bb579416059fa33d6b54dbc6
Mayank <mayankgpt185@gmail.com>
2025-05-04 02:08:48 +0530

    strongly typed

--- app/manage-exam/page.tsx
+++ app/manage-exam/page.tsx
@@ -161,7 +161,7 @@ const ExamCard = ({
 };
 
 interface ManageExamPageProps {
-    exams?: Exam[];
+    exams: Exam[];
 }
 
 export default function ManageExamPage({ exams = [] }: ManageExamPageProps) {
